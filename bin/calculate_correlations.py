e0e4bf3ae9c7f11c879bd0593d70819077752ecc
Dean Malmgren <dmalmgren@ideo.com>
2019-09-07 06:55:09 -0500

    correlations viz

--- bin/calculate_correlations.py
+++ bin/calculate_correlations.py
@@ -2,6 +2,10 @@
 import datetime
 import itertools
 
+import numpy as np
+import seaborn as sns
+import pandas as pd
+
 from boglehead.fund import load_all_funds
 
 
@@ -14,11 +18,24 @@ for fund in all_funds:
     d = datetime.datetime.strptime(fund.min_date(), "%Y-%m-%d").date()
     if d < min_date:
         available_funds.append(fund)
+n = len(available_funds)
 
 # calculate the pairwise historical correlations across all pairs of funds
-for fund1, fund2 in itertools.combinations(available_funds, 2):
-    rho = fund1.correlation(fund2)
-    print(fund1.symbol, fund2.symbol, fund1.min_date(), fund2.min_date(), rho)
+symbols = [fund.symbol for fund in available_funds]
+rho_ij = np.zeros((n, n))
+for i, j in itertools.combinations(range(n), 2):
+    fund_i, fund_j = available_funds[i], available_funds[j]
+    rho_ij[i,j] = rho_ij[j,i] = fund_i.correlation(fund_j)
+df_rho_ij = pd.DataFrame(rho_ij, symbols, symbols)
 
 # visualize the pairwise historical correlations
 # https://towardsdatascience.com/better-heatmaps-and-correlation-matrix-plots-in-python-41445d0f2bec
+fig = sns.clustermap(
+    df_rho_ij,
+    method="average",
+    vmin=-1, vmax=1, center=0,
+    cmap=sns.diverging_palette(20, 220, n=200),
+    xticklabels=1, yticklabels=1,
+    square=True
+)
+fig.savefig("tmp/correlations/clustermap.png")
